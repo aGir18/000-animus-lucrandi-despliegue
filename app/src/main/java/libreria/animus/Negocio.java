2ea18e62ed0115adef207429f2fa9b608696ea8f
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-04-29 19:16:39 +0100

    cambios

--- app/src/main/java/libreria/animus/Negocio.java
+++ app/src/main/java/libreria/animus/Negocio.java
@@ -1,25 +1,29 @@
 package libreria.animus;
 
-public abstract class Negocio {
+public abstract class Negocio implements NegocioIntLib {
 
 	private String nombre;
 	private String nif;
-	
-	public String getNif() {
-		return nif;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public void setNif(String nif) {
-		this.nif = nif;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+		
+  @Override
+  public String getNif() {
+    return nif;
+  }
+  
+  @Override
+  public String getNombre() {
+    return nombre;
+  }
+  
+  @Override
+  public void setNombre(String nombre) {
+    this.nombre = nombre;    
+  }
+  
+  @Override
+  public void setNif(String nif) {
+    this.nif = nif;
+  }
 	
 	public Negocio(String nombre, String nif){
 		this.nombre = nombre;
