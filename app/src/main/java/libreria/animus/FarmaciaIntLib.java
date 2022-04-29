4e101a62689110faae09e844f194193a7ea41ec3
Alejandro <alex.galez2218@gmail.com>
2022-04-29 10:27:10 +0200

    actualización interfaces

--- app/src/main/java/libreria/animus/FarmaciaIntLib.java
+++ app/src/main/java/libreria/animus/FarmaciaIntLib.java
@@ -2,4 +2,10 @@ package libreria.animus;
 
 public interface FarmaciaIntLib {
 
+	public int getNumeroPuntosSigre();
+	
+	public void setNumeroPuntosSigre(int numeroPuntosSigre);
+	
+	public String toString();
+	
 }
