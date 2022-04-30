9840bda5028929fcadbc71a1f944538b898b9008
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-04-30 20:23:23 +0100

    listas quitadas

--- app/src/main/java/libreria/animus/Asociacion.java
+++ app/src/main/java/libreria/animus/Asociacion.java
@@ -1,20 +1,20 @@
 package libreria.animus;
 
-import java.util.List;
+//import java.util.List;
 
 public class Asociacion {
 
   private long id;
 	private String nombre;
-	private List<Negocio> negociosAsociacion;
+	//private List<Negocio> negociosAsociacion;
 	
   public String getNombre() {
     return nombre;
   }
 	
-	public List<Negocio> getNegociosAsociacion() {
+	/*public List<Negocio> getNegociosAsociacion() {
 		return negociosAsociacion;
-	}
+	}*/
 	
 	public long getId() {
     return id;
@@ -23,24 +23,22 @@ public class Asociacion {
 	public void setNombre(String nombre) {
     this.nombre = nombre;
   }
-	
+	/*
 	public void setNegociosAsociacion(List<Negocio> negociosAsociacion) {
     this.negociosAsociacion = negociosAsociacion;
   }
-	
+	*/
 	public void setId(long id) {
     this.id = id;
   }
 
-	public Asociacion(String nombre, List<Negocio> negocios) {
+	public Asociacion(String nombre) {
 		this.nombre = nombre;
-		this.negociosAsociacion = negocios;
 	}
 	
 	@Override
 	public String toString() {
-		return "La asociacion se llama asociacion " + getNombre() + " y tiene " + 
-		    getNegociosAsociacion().size() + " negocios.";
+		return "La asociacion se llama asociacion " + getNombre() + " y tiene " + " negocios.";
 	}
 
 }
