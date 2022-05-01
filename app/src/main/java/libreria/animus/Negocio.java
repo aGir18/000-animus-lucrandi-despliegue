0a7eb8bc1a497833c449f22a1072814ebc44d301
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-05-01 20:52:29 +0100

    Cambio a anterior

--- app/src/main/java/libreria/animus/Negocio.java
+++ app/src/main/java/libreria/animus/Negocio.java
@@ -5,7 +5,6 @@ public abstract class Negocio {
   private long id;
 	private String nombre;
 	private String nif;
-	private Asociacion asociacion;
 		
 	public long getId() {
     return id;
@@ -19,10 +18,6 @@ public abstract class Negocio {
     return nombre;
   }
   
-  public Asociacion getAsociacion() {
-    return asociacion;
-  }
-  
   public void setNombre(String nombre) {
     this.nombre = nombre;    
   }
@@ -33,10 +28,6 @@ public abstract class Negocio {
 	
   public void setId(long id) {
     this.id = id;
-  }
-
-  public void setAsociacion(Asociacion asociacion) {
-    this.asociacion = asociacion;
   }
   
   public Negocio(String nombre, String nif){
