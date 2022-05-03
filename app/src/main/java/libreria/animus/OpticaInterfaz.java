14bebf6bec74b95ed1f49f30abfea059c00f66eb
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-05-03 08:41:21 +0100

    añado interfaz Optica

---
+++ app/src/main/java/libreria/animus/OpticaInterfaz.java
@@ -0,0 +1,9 @@
+package libreria.animus;
+
+public interface OpticaInterfaz {
+
+  public int getNumeroAutorefractometros();
+  
+  public void setNumeroAutorefractometros(int numeroAutorefractometros);
+  
+}
